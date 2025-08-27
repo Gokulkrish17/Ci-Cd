7b0c1cb2b762f0a7c200ec181af58719b027afe3
Gokulkrish17 <gokulgokul868@gmail.com>
2025-08-27 12:48:01 +0530

    Initial commit

--- src/main/java/com/demo/cicd/controller/StudentController.java
+++ src/main/java/com/demo/cicd/controller/StudentController.java
@@ -31,7 +31,7 @@ public class StudentController {
 
     @GetMapping("/message")
     public String getMessage(){
-        return "Welcome to ci-cd course, We will teach you CI_CD core concept";
+        return "Welcome to ci-cd course, We will teach you CI_CD core concept, Learn and Enjoy";
     }
 
     @GetMapping("/registered-list")
